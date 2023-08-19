6f85eb96cade16582ba439859ed59258431ee6c0
Bruce-Hopkins <behopkinsjr@gmail.com>
2023-08-19 10:48:12 +0200

    feat: top nav has accurate time

--- src/lib/components/layout/Topnav.svelte
+++ src/lib/components/layout/Topnav.svelte
@@ -1,14 +1,33 @@
+<script lang="ts">
+    import { getTimeFromDate } from "../../common/calculations";
+
+  let time = getTimeFromDate(new Date())
+  setInterval(changeTime, 1000 )
+  function changeTime() {
+    time = getTimeFromDate(new Date)
+  }
+</script>
+
 <div class="topnav">
-  <h2>25:33</h2>
+  <h2>{time}</h2>
 </div>
+<div class="topnav-slot"/>
+
 
 <style>
   .topnav {
     width: 100%;
-    height: 62px;
+    height: 61px;
     background-color: #404040;
     display: flex;
     justify-content: center;
     align-items: center;
+    border-bottom: #8F8F8F solid 1px;
+    position: fixed;
   }
+  .topnav-slot {
+    width: 100%;
+    height: 62px;
+  }
+  
 </style>
